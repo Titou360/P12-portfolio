2a1572519a1d8edcdc443c0d2194998502f4805e
Titou360 <clement.felices@gmail.com>
2023-12-11 18:43:18 +0100

    Corrections v1.1

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -7,13 +7,19 @@ const ProjectCard = ({ filteredCards }) => {
       {filteredCards.map((card) => (
         <div key={card.id} className="py-8 px-4">
           <div className="inline-grid h-full lg:flex bg-dark dark:bg-light dark:border-primary rounded-lg border shadow-lg pb-6 lg:pb-0 ">
-            <div className="w-full lg:w-1/3 lg:p-4">
-              <Image className="object-fill" src={card.cover} alt="Image du projet" width={150} height={150} />
+            <div className="w-full lg:w-1/3 p-4">
+              <Image
+                className="object-fill shadow-[5px_5px_0px_0px_rgba(229,88,18)]"
+                src={card.cover}
+                alt="Image du projet"
+                width={150}
+                height={150}
+              />
             </div>
 
             <div className="w-full lg:w-2/3 p-4">
               <div className="inline-grid">
-                <h2 className="font-semibold text-xl text-light dark:text-dark">{card.title}</h2>
+                <h2 className="font-semibold text-xl text-primary dark:text-dark">{card.title}</h2>
 
                 <p className="text-sm my-3 text-light dark:text-dark opacity-75">{card.explain}</p>
               </div>
@@ -33,7 +39,7 @@ const ProjectCard = ({ filteredCards }) => {
               </motion.a>
             ) : (
               <div className="text-dark py-3 px-4 rounded-lg bg-gray-300 cursor-not-allowed">
-                <p className="font-semibold text-sm tracking-wide">Démo ⛔</p>
+                <p className="font-semibold text-sm tracking-wide">Démo</p>
               </div>
             )}
 
@@ -50,7 +56,7 @@ const ProjectCard = ({ filteredCards }) => {
               </motion.a>
             ) : (
               <div className="py-3 px-4 bg-gray-300 rounded-lg ml-3 cursor-not-allowed">
-                <p className="font-semibold text-sm tracking-wide">Github ⛔</p>
+                <p className="font-semibold text-sm tracking-wide">Github</p>
               </div>
             )}
 
